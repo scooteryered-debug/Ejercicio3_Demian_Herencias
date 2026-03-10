11bde331ce38fbe32da9b315fccd35b2cbf4a5c3
Demian Mendoza <scooter.yered@gmail.com>
2026-03-10 13:41:46 -0600

    Version 2

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from clases.herencia_1.taxi import Taxi
+from clases.herencia_1.auto_particular import AutoParticular
 
 def main(): 
     coche = Taxi("123-GTO","Versa",1000,"123-a")
-
+    
+    print("****************")
     print(coche) 
     coche.encender() 
     coche.subirPasajeros() 
     coche.acelerar() 
     coche.frenar() 
     coche.cobrarCuota() 
+    
+    ap = AutoParticular("123", "Demian", 15, "Volvo", "Plata", "987-G")
+    print(ap)
+    ap.SubirseAuto()
+    ap.EncenderAuto()
+    ap.LlegarDestino()
+    ap.BajarseAuto()
 
 if __name__ == '__main__': 
     main()
